93852b8555c2fbc82f577f52e73b2d0d051eba3e
moham <moham@DESKTOP-GILB5EU>
2023-08-08 22:44:59 +0530

    Add project files.

---
+++ TechworldCL/Class1.cs
@@ -0,0 +1,55 @@
+﻿namespace TechworldCL
+{
+    public class instructor
+    {
+        public float avgFeedback;
+        public int experience;
+        public string? instructorName;
+        public string[] instructorSkill;
+
+        public instructor(float avgFeedback, int experience, string instructorName, string[] instructorSkill)
+        {
+            this.avgFeedback = avgFeedback;
+            this.experience = experience < 0 ? 1 : experience;
+            this.instructorName = instructorName;
+            this.instructorSkill = instructorSkill;
+        }
+
+        public bool validateEligibility()
+        {
+            if (experience > 3 && avgFeedback >= 4.5)
+            {
+                return true;
+
+            }
+            else if (experience < 3 && avgFeedback <= 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CheckSkill(string Technology)
+        {
+            Console.Write("Technology =");
+            Technology = Console.ReadLine();
+            foreach (string skill in instructorSkill)
+            {
+                if (Array.Exists(instructorSkill, skill => Technology.Equals(skill.Trim(), StringComparison.OrdinalIgnoreCase)))
+                {
+                    Console.WriteLine("you have skill is validated");
+                    return;
+                }
+            }
+
+                Console.WriteLine("your skill is not validated");
+                
+
+            
+
+        }
+    }
+}
---
+++ techworldapp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechworldCL;
+
+namespace techworldapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Feedback =");
+            float avgFeedback = float.Parse(Console.ReadLine());
+            Console.Write("Experience=");
+            int experience = int.Parse(Console.ReadLine());
+            Console.Write("Name=");
+            string instructorName = Console.ReadLine();
+            Console.Write("skill  =");
+            string inputSkill = Console.ReadLine();
+            string[] instructorSkill = inputSkill.Split(',');
+
+
+            instructor inst1 = new instructor(avgFeedback, experience, instructorName, instructorSkill);
+            bool elegibility = inst1.validateEligibility();
+            Console.WriteLine( elegibility);
+          string Technology = Console.ReadLine();
+           Technology = Technology.Trim();
+            inst1.CheckSkill(Technology);
+
+        }
+
+
+    }
+}
